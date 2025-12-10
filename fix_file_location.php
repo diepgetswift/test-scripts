1a9210d6e8c8378931ea801a06366e0e3bf2dc63
Bui Diep <dbui@getswift.co>
2025-12-10 23:30:53 +0700

    [+] add script fix file location

---
+++ fix_file_location.php
@@ -0,0 +1,22 @@
+<?php
+require "../auth.php";
+
+$images = func_query("select * from dbp_thumbnails");
+
+$count = 1;
+foreach ($images as $i) {
+    echo $count ++ . " ";
+    $path = $i['image_path'];
+    if (file_exists($dbp_dir . $path)) {
+        echo 'file existed';
+    } else {
+        $newPath = str_replace('/files', '/files/Product Tile Images', $path);
+        if (file_exists($dbp_dir . $newPath)) {
+            echo 'file moved';
+        } else {
+            echo 'file deleted';
+        }
+    }
+    
+    echo PHP_EOL;
+}
--- test_daily_update_stat.php
+++ test_daily_update_stat.php
@@ -3,12 +3,12 @@ require "../auth.php";
 
 echo "start " . date('H:i:s') . PHP_EOL;
 
-$customers = func_query("SELECT tdc.customer_id, c.login from dbp_today_delivery_customers inner join dbp_customers c ON tdc.customer_id = c.customer_id");
+$customers = func_query("SELECT tdc.customer_id, c.login from dbp_today_delivery_customers tdc inner join dbp_customers c ON tdc.customer_id = c.customer_id");
 
 $count = 1;
 foreach ($customers as $c) {
     echo $count ++;
-    $stat = func_query_first("select count(*) as count, sum(subtotal) as sum from dbp_orders where login = {$c['login']}");
+    $stat = func_query_first("select count(*) as count, sum(subtotal) as sum from dbp_orders where login = '{$c['login']}'");
     db_query("UPDATE dbp_customer_stats SET orders_count = {$stat['count']}, lifetime_value = {$stat['sum']} where customer_id = {$c['customer_id']}");
     db_query("DELETE FROM dbp_today_delivery_customers WHERE customer_id = {$c['customer_id']}");
     
