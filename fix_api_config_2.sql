3d4eaa99f52ce7257b21b67db3674756b2f3cf04
Bui Diep <dbui@getswift.co>
2026-07-02 12:17:34 +0700

    [*] change config for industry

--- fix_api_config_2.sql
+++ fix_api_config_2.sql
@@ -1,4 +1,4 @@
 update dbp_config set value = 'live' where name = 'tenant_env';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_live';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_staging';
-update dbp_config set value = '019f194f-41df-7095-9e73-506c0807a825' where name = 'tenant_uuid';
+update dbp_config set value = '019f1957-3336-72bb-b88e-e82a8b6ee073' where name = 'tenant_uuid';
