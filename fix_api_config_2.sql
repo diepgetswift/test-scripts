c54d1dc8641dc1c26f11fbabe1bc3dda53e4ad3c
Bui Diep <dbui@getswift.co>
2026-07-02 10:46:05 +0700

    [+] add file to fix api config

--- fix_api_config_2.sql
+++ fix_api_config_2.sql
@@ -1,3 +1,5 @@
 update dbp_config set value = 'live' where name = 'tenant_env';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_live';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_staging';
+    
+update dbp_config set value = '019f194f-41df-7095-9e73-506c0807a825' where name = 'tenant_uuid';
