1d67e87128ff45dbe50c050a425e6ba62285831b
Bui Diep <dbui@getswift.co>
2026-07-02 12:20:57 +0700

    [*] change config for bashpos

--- fix_api_config_2.sql
+++ fix_api_config_2.sql
@@ -1,4 +1,4 @@
 update dbp_config set value = 'live' where name = 'tenant_env';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_live';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_staging';
-update dbp_config set value = '019f195e-e22b-71df-9335-49a781855aa6' where name = 'tenant_uuid';
+update dbp_config set value = '019f1959-ec68-73f8-a6a7-183040593b92' where name = 'tenant_uuid';
