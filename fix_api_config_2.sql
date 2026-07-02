47156cce3bb67a3f29b549f5a51e2f1dfc219d48
Bui Diep <dbui@getswift.co>
2026-07-02 12:19:16 +0700

    [*] change config for pinepos

--- fix_api_config_2.sql
+++ fix_api_config_2.sql
@@ -1,4 +1,4 @@
 update dbp_config set value = 'live' where name = 'tenant_env';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_live';
 update dbp_config set value = 'https://auth.staging.deliverybizpro.com/' where name = 'tenant_service_url_staging';
-update dbp_config set value = '019f1957-3336-72bb-b88e-e82a8b6ee073' where name = 'tenant_uuid';
+update dbp_config set value = '019f195e-e22b-71df-9335-49a781855aa6' where name = 'tenant_uuid';
